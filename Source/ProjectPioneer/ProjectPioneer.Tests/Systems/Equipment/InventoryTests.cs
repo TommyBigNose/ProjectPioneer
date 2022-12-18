23a9e08028b261182b98c284cdaa19f3502c343d
ZERO-COOL\thoma <thomaslachin89@gmail.com>
2022-12-18 11:31:57 -0600

    prep for shop and inventory

---
+++ Source/ProjectPioneer/ProjectPioneer.Tests/Systems/Equipment/InventoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPioneer.Tests.Systems.Equipment
+{
+	[TestFixture]
+	public class InventoryTests
+	{
+		[SetUp]
+		public void SetUp()
+		{
+
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+
+		}
+
+		[Test]
+		public void Should_Something()
+		{
+			// Arrange
+			// Act
+			// Assert
+		}
+	}
+}
