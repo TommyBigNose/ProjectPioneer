572f3162f60f9288e26758c229966a074a3fbd25
ZERO-COOL\thoma <thomaslachin89@gmail.com>
2023-01-07 12:59:33 -0600

    Getting ready for Game and the crap ton of tests that will exist for it

---
+++ Source/ProjectPioneer/ProjectPioneer.Tests/Systems/GameTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectPioneer.Systems;
+using ProjectPioneer.Systems.Data;
+using ProjectPioneer.Systems.Equipment;
+
+namespace ProjectPioneer.Tests.Systems
+{
+	[TestFixture]
+	public class GameTests
+	{
+		private IDataSource _dataSource;
+		private IGame _sut;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_dataSource = new LocalDataSource();
+			_sut = new Game(_dataSource);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+
+		}
+
+		[Test]
+		public void Should_Pass_When_Valid()
+		{
+			// Arrange
+			// Act
+			var result = _sut;
+
+			// Assert
+			Assert.That(result, Is.Not.Null, "Result was null");
+		}
+	}
+}
---
+++ Source/ProjectPioneer/ProjectPioneer/Systems/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectPioneer.Systems.Data;
+
+namespace ProjectPioneer.Systems
+{
+	public class Game : IGame
+	{
+		private readonly IDataSource _dataSource;
+
+		public Game(IDataSource dataSource)
+		{
+			_dataSource = dataSource;
+		}
+	}
+}
