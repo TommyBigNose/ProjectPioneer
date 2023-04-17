b08eb713dac95f07a552d09343193e6642db6b2f
Thomas L <thomaslachin89@gmail.com>
2023-04-17 18:14:55 -0500

    Starting standard button across all pages, I want audio on click for everything

---
+++ Source/ProjectPioneer/ProjectPioneer.Blazor/Utility/AudioFxHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectPioneer.Blazor.Utility
+{
+	public static class AudioFxHelper
+	{
+		public static string Fx_Positive_Url =  "/sounds/Windows_Balloon.wav";
+		public static string Fx_Negative_Url =  "/sounds/Windows_Notify.wav";
+		public static string Fx_Success_Url =  "/sounds/Windows_Logon.wav";
+		public static string Fx_Cheer_Url =  "/sounds/Windows_Tada.wav";
+		public static string Fx_Failure_Url =  "/sounds/Windows_Ding.wav";
+		public static string Fx_Unknown_Url =  "/sounds/Windows_Ding.wav";
+		
+	}
+}
