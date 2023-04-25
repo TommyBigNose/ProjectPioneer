e48159db63a4c5bd9410050bf57ab996d9b32b5f
Thomas L <thomaslachin89@gmail.com>
2023-04-25 17:31:39 -0500

    New click sound

--- Source/ProjectPioneer/ProjectPioneer.Blazor/Utility/AudioFxHelper.cs
+++ Source/ProjectPioneer/ProjectPioneer.Blazor/Utility/AudioFxHelper.cs
@@ -8,6 +8,7 @@
 		public static string Fx_Cheer_Url =  "/sounds/Windows_Tada.wav";
 		public static string Fx_Failure_Url =  "/sounds/Windows_Ding.wav";
 		public static string Fx_Unknown_Url =  "/sounds/Windows_Ding.wav";
+		public static string Fx_Info_Url =  "/sounds/Windows_Information_Bar.wav";
 		
 	}
 }
