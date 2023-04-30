5cfb929711efe431f153eed4b12d5666d6e3bc39
Thomas L <thomaslachin89@gmail.com>
2023-04-30 10:53:17 -0500

    Planning some stuff for the Main layout

--- Source/ProjectPioneer/ProjectPioneer.Blazor/Usings.cs
+++ Source/ProjectPioneer/ProjectPioneer.Blazor/Usings.cs
@@ -4,6 +4,7 @@ global using System.ComponentModel.DataAnnotations;
 global using System.Text.RegularExpressions;
 global using System.Timers;
 global using ProjectPioneer.Blazor.Models;
+global using ProjectPioneer.Blazor.Utility;
 global using ProjectPioneer.Systems.Adventure;
 global using ProjectPioneer.Systems.Character;
 global using ProjectPioneer.Systems.Equipment;
