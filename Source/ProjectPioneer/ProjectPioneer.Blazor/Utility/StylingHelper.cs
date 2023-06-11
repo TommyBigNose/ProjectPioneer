e058977108bcce2efa46627783af587a0a3d82fd
Thomas L <thomaslachin89@gmail.com>
2023-06-11 14:22:26 -0500

    Cleaning up button styling across the board

---
+++ Source/ProjectPioneer/ProjectPioneer.Blazor/Utility/StylingHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectPioneer.Blazor.Utility
+{
+	public static class StylingHelper
+	{
+		public static string Btn_Class_Primary = "btn-primary";
+		public static string Btn_Class_Secondary = "btn-secondary";
+		public static string Btn_Class_Success = "btn-success";
+		public static string Btn_Class_Danger = "btn-danger";
+		public static string Btn_Class_Warning = "btn-warning";
+		public static string Btn_Class_Info = "btn-info";
+		public static string Btn_Class_Link = "btn-link";
+		public static string Btn_Class_Light = "btn-light";
+		public static string Btn_Class_Dark = "btn-dark";
+	}
+}
