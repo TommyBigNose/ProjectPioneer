d76a781c8560a0e7e6308c2f5e35c22bb4287ed0
Thomas L <thomaslachin89@gmail.com>
2023-06-13 18:25:30 -0500

    Fx dictionary of times, not complete yet

--- Source/ProjectPioneer/ProjectPioneer.Blazor/Utility/AudioFxHelper.cs
+++ Source/ProjectPioneer/ProjectPioneer.Blazor/Utility/AudioFxHelper.cs
@@ -7,10 +7,22 @@
 		public static string Fx_Success_Url =  "/sounds/Windows_Logon.wav";
 		public static string Fx_Cheer_Url =  "/sounds/Windows_Tada.wav";
 		public static string Fx_Failure_Url =  "/sounds/Windows_Ding.wav";
-		public static string Fx_Unknown_Url =  "/sounds/Windows_Ding.wav";
+		// public static string Fx_Unknown_Url =  "/sounds/Windows_Ding.wav";
 		public static string Fx_Info_Url =  "/sounds/Windows_Information_Bar.wav";
 		public static string Fx_Calm_Url =  "/sounds/Windows_Ring9.wav";
 		public static string Fx_Shop_Beep_Url =  "/sounds/Repeatable_Beep.wav";
-		
+
+		public static readonly Dictionary<string, int> FxTimerDetails = new Dictionary<string, int>()
+		{
+			{Fx_Positive_Url, 1000},
+			{Fx_Negative_Url, 1000},
+			{Fx_Success_Url, 1000},
+			{Fx_Cheer_Url, 1000},
+			{Fx_Failure_Url, 1000},
+			// {Fx_Unknown_Url, 1000},
+			{Fx_Info_Url, 1000},
+			{Fx_Calm_Url, 1000},
+			{Fx_Shop_Beep_Url, 1000},
+		};
 	}
 }
