53deccd2ddfc9f450a192f61241a3a0cc9852516
Thomas L <thomaslachin89@gmail.com>
2023-06-18 07:27:25 -0500

    Moved AudioWrapper to models and general cleanup

---
+++ Source/ProjectPioneer/ProjectPioneer.Blazor/Models/AudioWrapper.cs
@@ -0,0 +1,9 @@
+﻿using ProjectPioneer.Blazor.Pages.Elements;
+
+namespace ProjectPioneer.Blazor.Models
+{
+	public class AudioWrapper
+	{
+		public AudioFxPlayer AudioFxComponent { get; set; }
+	}
+}
